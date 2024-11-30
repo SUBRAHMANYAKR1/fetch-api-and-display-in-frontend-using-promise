22ed0756c3605fac5434f9b8e619f6c1e189e6c3
Subrahmanya K R <152642284+SUBRAHMANYAKR1@users.noreply.github.com>
2024-12-01 00:44:03 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,9 @@
-fetch using asynchronous funtion using await and async
+// fetch using asynchronous funtion using await and async
+
+let data=fetch('https://github.com/users')
+data.then((respose)=>{
+  finaldata=response.json()
+  finaldata.then((result)=>{
+    console.log(result)
+  })
+})
